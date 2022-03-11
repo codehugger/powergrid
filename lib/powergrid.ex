143bc8f036d9fd7673ef9b4fb3416ca0e5544b51
Bjarki Gudlaugsson <bjarkigud@gmail.com>
2022-03-11 09:30:43 +0000

    First commit

---
+++ lib/powergrid.ex
@@ -0,0 +1,18 @@
+defmodule Powergrid do
+  @moduledoc """
+  Documentation for `Powergrid`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Powergrid.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/powergrid_test.exs
@@ -0,0 +1,8 @@
+defmodule PowergridTest do
+  use ExUnit.Case
+  doctest Powergrid
+
+  test "greets the world" do
+    assert Powergrid.hello() == :world
+  end
+end
